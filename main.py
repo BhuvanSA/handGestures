daf8f7f2f716893eca980ed6a09fc8e6dd63a9a4
Bhuvan S A <bhuvansa@Bhuvans-MacBook-Air.local>
2023-03-29 10:54:24 +0530

    fasd

--- main.py
+++ main.py
@@ -9,11 +9,19 @@ import numpy as np
 import mediapipe as mp
 import tensorflow as tf
 from tensorflow.keras.models import load_model
+from _ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 # initialize mediapipe
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 mpDraw = mp.solutions.drawing_utils
+
+# init other libs
+devices = AudioUtilities.GetSpeakers()
+interface = devices. Activate(IAudioEndpointVolume.__iid__, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
 
 # Load the gesture recognizer model
 model = load_model('mp_hand_gesture')
@@ -44,6 +52,7 @@ while True:
     # print(result)
 
     className = ''
+    prev = ''
 
     # post process the result
     if result.multi_hand_landmarks:
@@ -63,20 +72,36 @@ while True:
             prediction = model.predict([landmarks])
             # print(prediction)
             classID = np.argmax(prediction)
-            
+
             className = classNames[classID]
-            print(className)
-            if className == 'smile':
-                webbrowser.open('https://youtu.be/uHgt8giw1LY', new=2)
-                time.sleep(5)
 
+        # show the prediction on the frame
+        cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                    1, (0, 0, 255), 2, cv2.LINE_AA)
 
-    # show the prediction on the frame
-    # cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 
-                   # 1, (0,0,255), 2, cv2.LINE_AA)
+        if className == 'okay' and prev != 'okay':
+            prev = 'okay'
+            webbrowser.open('https://www.youtube.com/@gat_aiml', new=2)
+            time.sleep(4)
+
+        elif className == 'thumbs up' and prev != 'thumbs up':
+            prev = 'thumbs up'
+            # increase volume
+            volume.SetMasterVolumeLevel(-25.0, None)
+            time.sleep(1)
+
+        elif className == 'thumbs down' and prev != 'thumbs down':
+            prev = 'thumbs down'
+            # increase volume
+            time.sleep(1)
+
+        elif (className == 'rock' or className == 'fist') and (prev != 'thumbs down'):
+            prev = 'rock'
+            # increase volume
+            time.sleep(1)
 
     # Show the final output
-    cv2.imshow("Output", frame) 
+    cv2.imshow("Output", frame)
 
     if cv2.waitKey(1) == ord('q'):
         break
